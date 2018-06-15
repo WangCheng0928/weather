6dfb42517beac82b53e8ca3ac1d3a154aec10398
WangCheng0928 <493475850@qq.com>
2018-06-15 13:29:18 +0800

    commit feign

--- msa-weather-report-eureka-feign/src/main/java/com/wang/spring/clound/weather/service/WeatherDataClient.java
+++ msa-weather-report-eureka-feign/src/main/java/com/wang/spring/clound/weather/service/WeatherDataClient.java
@@ -1,6 +1,7 @@
 package com.wang.spring.clound.weather.service;
 
 import com.wang.spring.clound.weather.vo.WeatherResponse;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
